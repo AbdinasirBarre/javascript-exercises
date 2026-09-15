58f60eab3380fec700fd0a6314303606693f8f42
AbdinasirBarre <abarre333@gmail.com>
2026-09-15 20:30:42 +0100

    foundation js 08 practice completed: - completed remove from array function using filter

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+
+    let removed = array.filter(remove);
+    
+
+    function remove(value){
+        return !num.includes(value)
+    }
+
+    return removed;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+//     let removed = array.filter(remove);
+    
+
+//     function remove(value){
+//         return value !== num && value !== num2 && value !== num3 && value !== num4;
+//     }
+
+//     return removed;
+// };
